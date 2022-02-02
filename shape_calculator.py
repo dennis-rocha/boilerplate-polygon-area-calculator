11cdb30cbf2346d75dd623ae81555523007bd1fd
dennis.rocha <dennis.rocha@neoway.com.br>
2022-02-02 17:15:49 -0300

    Arquivo contento o teste finalizado

---
+++ shape_calculator.py
@@ -0,0 +1,44 @@
+class Rectangle:
+    def __init__(self, width, height) -> None:
+        self.width = width
+        self.height = height
+        
+    def __str__(self) -> str:
+        return f"Rectangle(width={self.width}, height={self.height})"
+
+
+        
+    def set_width (self, width):
+        self.width = width
+
+    def set_height (self, height):
+        self.height = height
+
+    def get_area (self):
+        return self.width * self.height
+
+    def get_perimeter (self):
+        return 2 * self.width + 2 * self.height
+
+    def get_diagonal (self):
+        return (self.width ** 2 + self.height ** 2) ** .5
+
+    def get_picture (self):
+        filter = self.height > 50 or self.width > 50 
+        return "Too big for picture." if filter else ("*"*self.width + "\n") * self.height 
+
+    def get_amount_inside (self, shape):
+        return int(self.get_area() / shape.get_area())
+    
+    
+class Square(Rectangle):
+    def __init__(self, side) -> None:
+        self.width = side
+        self.height = side
+        
+    def __str__(self) -> str:
+        return f"Square(side={self.width})"
+        
+    def set_side (self, side):
+        self.width = side
+        self.height = side
